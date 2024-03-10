afb1a9634260221b8b36f52bc6e6b78be85e9f51
Wian Stipp <stippwian@gmail.com>
2024-03-09 22:33:18 -0800

    writing

--- examples/anatomy.py
+++ examples/anatomy.py
@@ -3,18 +3,32 @@
 import configmate
 import os
 import fire
+import tqdm
 
-from papyrusai import models, reading
+from papyrusai import models, reading, writing
 
 
-def main(image_path: os.PathLike) -> None:
+def main(input_folder: os.PathLike, output_folder: os.PathLike) -> None:
+    for path in tqdm.tqdm(os.listdir(input_folder)):
+        if not path.endswith(("jpg", "png", "jpeg")):
+            continue
+        fullpath = os.path.join(input_folder, path)
+        output_fn = f"output_{path.split('.')[0]}.txt"
+        output_path = os.path.join(output_folder, output_fn)
+        if output_fn in os.listdir(output_folder):
+            continue
+        convert_image_from_path(fullpath, output_path)
+
+
+def convert_image_from_path(image_path: os.PathLike, save_path: os.PathLike) -> None:
     config = configmate.get_config(
         "config/anatomy.yaml",
         section="AnatomyAndPhysiologyTextbook",
         validation=models.ImageToTextPrompt,
     )
+    writer = writing.FileWriter(save_path)
     reader = reading.GPTImageReader(image_path, config.prompt())
-    print(reader.read())
+    writer.write(reader.read())
 
 
 if __name__ == "__main__":
---
+++ papyrusai/writing.py
@@ -0,0 +1,19 @@
+"""This module contains code to output the extracted text into various outputs."""
+
+import os
+import abc
+
+
+class Writer(abc.ABC):
+    @abc.abstractmethod
+    def write(self, content: str) -> None: ...
+
+
+class FileWriter(Writer):
+    def __init__(self, write_path: os.PathLike) -> None:
+        super().__init__()
+        self.write_path = write_path
+
+    def write(self, content: str) -> None:
+        with open(self.write_path, "w") as f:
+            f.write(content)
